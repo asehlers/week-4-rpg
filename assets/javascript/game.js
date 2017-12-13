a78be830de9c1174c0bbabceaa818159718197e3
Andrew Ehlers <asehlers17@gmail.com>
2017-12-13 16:13:11 -0500

    prebattle phase finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,105 +1,178 @@
 var gameStarted=false;
-var defender=false;
-
+var defender = false;
+var attacker = "";
+var characters = [];
+var chosenDefender;
 window.onload = function() {
 
   //  Click events are done for us:
   $("#pink").click(pinkSlime.slimeClicked);
-  $("#puddle").click(puddleSlime.slimeClicked);;
-  // $("#radSlime").click();
-  // $("#boomSlime").click();
-
+  $("#puddle").click(puddleSlime.slimeClicked);
+  $("#rad").click(radSlime.slimeClicked);
+  $("#boom").click(boomSlime.slimeClicked);
 };
 
-var pinkSlime = {
-	health: 200,
-	attack: 5,
-	counterAttack: 10,
-	totalAttack: 0,
-	image: "assets/images/pinkSlime.jpg",
+function Slime(nameInput, healthInput, attackInput, counterAttackInput, imageInput){
+	this.health = healthInput;
+	this.attack = attackInput;
+	this.counterAttack = counterAttackInput;
+	this.totalAttack = 0;
+	this.image = imageInput;
+	this.name = nameInput;
+	var self = this;
 
-	initializeSlime: function(){
-		console.log("initialize pink");
-		var pinkImage = $("<img>");
-		pinkImage.attr("src", pinkSlime.image);
-		pinkImage.attr("id", "pink");
-		$("#availableCharacters").append(pinkImage);
-	},
+	this.initializeSlime = function(){
+		console.log("initialize slime");
+		var slimeImage = $("<img>");
+		slimeImage.attr("src", self.image);
+		slimeImage.attr("id", self.name);
+		$("#availableCharacters").append(slimeImage);
+	};
 
-	slimeClicked: function(){
+	this.slimeClicked = function(){
 		if(!gameStarted){
 			console.log("slimeclicked before game start");
-			$("#yourCharacter").append($("#pink"));
-			$("#enemyCharacters").append($("#puddle"));
+			$("#yourCharacter").append($("#" + self.name));
+			// $("#enemyCharacters").append($("#puddle"));
 			gameStarted = true;
+			console.log(characters.indexOf(self));
+			characters.splice(characters.indexOf(self), 1);
+			for(var i = 0; i < characters.length; i++){
+				console.log("enemy" + characters[i].name);
+				characters[i].makeEnemy();
+			}
 		}
 		else if(!defender){
 				console.log("slime clicked to select enemy");
-				$("#currentDefender").append($("#pink"));
+				$("#currentDefender").append($("#" + self.name));
 				defender = true;
+				chosenDefender = self;
 		}
-	},
-	attackEnemy: function(enemy){
-		pinkSlime.totalAttack += pinkSlime.attack;
-		enemy.health -= pinkSlime.totalAttack;
-		pinkSlime.health -= enemy.counterAttack;
-		console.log("Pink slime Health/attack: " + pinkSlime.health + " " + pinkSlime.totalAttack);
-		console.log("enemy health " + enemy.health);
-	}
+	};
 
-}
+	this.makeEnemy = function(){
+		// console.log("enemy make");
+		$("#enemyCharacters").append($("#" + self.name));
+	};
 
-var puddleSlime = {
-	health: 180,
-	attack: 7,
-	counterAttack: 15,
-	totalAttack: 0,
-	image: "assets/images/puddleSlime.jpg",
-
-	initializeSlime: function(){
-		console.log("initialize puddle");
-		var puddleImage = $("<img>");
-		puddleImage.attr("src", puddleSlime.image);
-		puddleImage.attr("id", "puddle");
-		$("#availableCharacters").append(puddleImage);
-	},
-
-	slimeClicked: function(){
-		if(!gameStarted){
-			console.log("slime clicked before game start");
-			$("#yourCharacter").append($("#puddle"));
-			$("#enemyCharacters").append($("#pink"));
-			gameStarted = true;
-		}
-		else{
-			if(!defender){
-				console.log("slime clicked to select enemy");
-				$("#currentDefender").append($("#puddle"));
-				defender = true;
-			}
-		}
+	this.attackEnemy(enemySlime){
+		
 	}
 }
 
-var charactesr = {pink:pinkSlime, puddle:puddleSlime};
+// var pinkSlime = {
+// 	health: 200,
+// 	attack: 5,
+// 	counterAttack: 10,
+// 	totalAttack: 0,
+// 	image: "assets/images/pinkSlime.jpg",
 
+// 	initializeSlime: function(){
+// 		console.log("initialize pink");
+// 		var pinkImage = $("<img>");
+// 		pinkImage.attr("src", pinkSlime.image);
+// 		pinkImage.attr("id", "pink");
+// 		$("#availableCharacters").append(pinkImage);
+// 	},
+
+// 	slimeClicked: function(){
+// 		if(!gameStarted){
+// 			console.log("slimeclicked before game start");
+// 			$("#yourCharacter").append($("#pink"));
+// 			$("#enemyCharacters").append($("#puddle"));
+// 			gameStarted = true;
+// 			attacker = "pink";
+// 		}
+// 		else if(!defender){
+// 				console.log("slime clicked to select enemy");
+// 				$("#currentDefender").append($("#pink"));
+// 				defender = "pink";
+// 		}
+// 	},
+
+
+
+// }
+
+// var puddleSlime = {
+// 	health: 180,
+// 	attack: 7,
+// 	counterAttack: 15,
+// 	totalAttack: 0,
+// 	image: "assets/images/puddleSlime.jpg",
+
+// 	initializeSlime: function(){
+// 		console.log("initialize puddle");
+// 		var puddleImage = $("<img>");
+// 		puddleImage.attr("src", puddleSlime.image);
+// 		puddleImage.attr("id", "puddle");
+// 		$("#availableCharacters").append(puddleImage);
+// 	},
+
+// 	slimeClicked: function(){
+// 		if(!gameStarted){
+// 			console.log("slime clicked before game start");
+// 			$("#yourCharacter").append($("#puddle"));
+// 			$("#enemyCharacters").append($("#pink"));
+// 			gameStarted = true;
+// 		}
+// 		else{
+// 			if(!defender){
+// 				console.log("slime clicked to select enemy");
+// 				$("#currentDefender").append($("#puddle"));
+// 				defender = true;
+// 			}
+// 		}
+// 	}
+// }
+
+// var characters = {pink:pinkSlime, puddle:puddleSlime};
+
+
+	// attackEnemy: function(enemy){
+	// 	pinkSlime.totalAttack += pinkSlime.attack;
+	// 	enemy.health -= pinkSlime.totalAttack;
+	// 	pinkSlime.health -= enemy.counterAttack;
+	// 	console.log("Pink slime Health/attack: " + pinkSlime.health + " " + pinkSlime.totalAttack);
+	// 	console.log("enemy health " + enemy.health);
+	// }
+
+	
 // var characters = {"pink":};
 $("#attackButton").on("click", function(){
 	console.log("attack button pressed");
 	if(gameStarted && defender){
 		console.log("fight is on");
-		console.log($("#yourCharacter:nth-child(2)"))
-		if($("#yourCharacter:nth-child(2)").attr("id") === "pink"){
-			conosle.log("pink attacker");
-			if($("#currentDefender:nth-child(2)").attr("id") === "puddle"){
-				console.log("puddle defender");
-				pinkSlime.attackEnemy(puddleSlime);
-			}
-		}
+
+
+		// console.log($("#yourCharacter:nth-child(2)"))
+		// if($("#yourCharacter:nth-child(2)").attr("id") === "pink"){
+		// 	conosle.log("pink attacker");
+		// 	if($("#currentDefender:nth-child(2)").attr("id") === "puddle"){
+		// 		console.log("puddle defender");
+		// 		pinkSlime.attackEnemy(puddleSlime);
+		// 	}
+		// }
+
+
 	}
 });
 
 console.log("pre initialize");
+var pinkSlime = new Slime("pink", 200, 5, 10, "assets/images/pinkSlime.jpg");
 pinkSlime.initializeSlime();
-puddleSlime.initializeSlime();
+characters.push(pinkSlime);
 
+var puddleSlime = new Slime("puddle", 180, 7, 15, "assets/images/puddleSlime.jpg");
+puddleSlime.initializeSlime();
+characters.push(puddleSlime);
+
+var radSlime = new Slime("rad", 150, 10, 20, "assets/images/radSlime.jpg");
+radSlime.initializeSlime();
+characters.push(radSlime);
+
+var boomSlime = new Slime("boom", 120, 20, 25, "assets/images/boomSlime.jpg");
+boomSlime.initializeSlime();
+characters.push(boomSlime);
+
+console.log(characters);
